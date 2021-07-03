52210e0f48bf82beb286eddae59d27f73e3b8656
sujit420 <sujit.89.kmr@gmail.com>
2021-07-03 22:15:38 +0530

    importing new tennis python file

--- python/tennis_test.py
+++ python/tennis_test.py
@@ -2,14 +2,15 @@
 
 import pytest
 from tennis import TennisGame1, TennisGame2, TennisGame3
-
+from tennis_new import Tennis
 from tennis_unittest import test_cases, play_game
 
 class TestTennis:
 
     @pytest.mark.parametrize('p1Points p2Points score p1Name p2Name'.split(), test_cases)
     def test_get_score_game1(self, p1Points, p2Points, score, p1Name, p2Name):
-        game = play_game(TennisGame1, p1Points, p2Points, p1Name, p2Name)
+        game = play_game(Tennis, p1Points, p2Points, p1Name, p2Name)
+        #game = play_game(TennisGame1, p1Points, p2Points, p1Name, p2Name)
         assert score == game.score()
 
     @pytest.mark.parametrize('p1Points p2Points score p1Name p2Name'.split(), test_cases)
